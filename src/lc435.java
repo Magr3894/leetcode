122318426f9a4ed3cc8dd81ee26274e7fea06756
maguorong <617044423@qq.com>
2020-12-31 14:59:46 +0800

    20201231每日打卡435

--- src/lc435.java
+++ src/lc435.java
@@ -1,6 +1,43 @@
+import java.util.*;
+
 public class lc435 {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+//        int[][] input = { {1,2},{2,3}, {3,4}, {1,3} };
+        int[][] input = { {1,2},{2,3} };
+        lc435 lc = new lc435();
+        System.out.println(lc.eraseOverlapIntervals(input));
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals)
+    {
+        if(intervals.length==0)return 0;
+
+        int ans = 0;
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1]>o2[1])
+                    return 1;
+                else if(o1[1]<o2[1])
+                    return -1;
+
+                return 0;
+            }
+        });
+
+        int right = intervals[0][1];
+        for(int i=1;i<intervals.length;i++)
+        {
+            if(intervals[i][0]>=right)
+            {
+                right = intervals[i][1];
+            }else
+            {
+                ans++;
+            }
+        }
+        return ans;
     }
 }
